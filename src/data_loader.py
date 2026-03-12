34a9b4ef02e05d081877a2b1e66416effbdf99bc
DhruvVerma8291 <dhruvverma2898@gmail.com>
2026-03-12 13:51:08 +0530

    Create data_loader.py

---
+++ src/data_loader.py
@@ -0,0 +1,33 @@
+import tensorflow as tf
+import os
+
+IMAGE_SIZE = (224, 224)
+BATCH_SIZE = 32
+
+
+def load_datasets(data_dir):
+
+    train_ds = tf.keras.preprocessing.image_dataset_from_directory(
+        data_dir,
+        validation_split=0.2,
+        subset="training",
+        seed=42,
+        image_size=IMAGE_SIZE,
+        batch_size=BATCH_SIZE
+    )
+
+    val_ds = tf.keras.preprocessing.image_dataset_from_directory(
+        data_dir,
+        validation_split=0.2,
+        subset="validation",
+        seed=42,
+        image_size=IMAGE_SIZE,
+        batch_size=BATCH_SIZE
+    )
+
+    AUTOTUNE = tf.data.AUTOTUNE
+
+    train_ds = train_ds.prefetch(buffer_size=AUTOTUNE)
+    val_ds = val_ds.prefetch(buffer_size=AUTOTUNE)
+
+    return train_ds, val_ds
